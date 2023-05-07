65d5f5a45d997a86d694e14657b7fb373c7417ec
pustart <polaris9955@gmail.com>
2023-05-07 17:38:15 +0300

    feat: add swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
 
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("MoneyPie API")
+    .setDescription("MoneyPie app API description")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
