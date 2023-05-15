239582bd64f6a0d19ebac3e4a350cb1a6cab6f94
pustart <polaris9955@gmail.com>
2023-05-15 12:43:16 +0300

    feat: add group module with table model

---
+++ src/groups/groups.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { GroupsService } from "./groups.service";
+import { GroupsController } from "./groups.controller";
+import { SequelizeModule } from "@nestjs/sequelize";
+import Group from "./models/group.model";
+import { Transaction } from "src/transactions/models/transaction.model";
+
+@Module({
+  controllers: [GroupsController],
+  providers: [GroupsService],
+  imports: [SequelizeModule.forFeature([Group, Transaction])],
+})
+export class GroupsModule {}
