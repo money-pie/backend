57ce11c5fd26af1329c41bfb4ff79c016667f65b
pustart <polaris9955@gmail.com>
2023-06-05 17:03:14 +0300

    fix: add CORS policy

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CLIENT,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  });
+
   const config = new DocumentBuilder()
     .setTitle("MoneyPie API")
     .setDescription("MoneyPie app REST API documentation")
